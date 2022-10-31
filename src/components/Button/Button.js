0c92fdcbd593da57141e135616e53bef45ba6e9f
Maksym Hlanskov <makfiz@gmail.com>
2022-10-31 21:37:45 +0200

    Update Button.js

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,5 @@
 import { Btn } from 'components/Button/Button.styled'
-import { Box } from "components/utils/Box";
+
 export const Button = ({ onClickBtn }) => {
     return <Btn type='button' onClick={() => onClickBtn()}>Load more</Btn> 
 }
